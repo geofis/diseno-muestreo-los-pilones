1777750ae465dfd1e90e8b552843d2cc6078e615
geofis <joseramon@geografiafisica.org>
2023-04-15 12:34:47 -0400

    UPDATE first html, ga_script

--- R/funciones.R
+++ R/funciones.R
@@ -1,61 +1,25 @@
-library(sf)
-library(tidyverse)
-h3_res10 <- st_read('fuentes/h3-resolucion-10.gpkg')
-h3_res10_c <- st_centroid(h3_res10)
-h3_res10_v <- h3_res10 %>% st_cast('POINT')
-h3_res10_
-d[1,] %>% st_centroid %>% ggplot + geom_sf(size = 0.25) + geom_sf(data = d[1,] %>% st_cast('LINESTRING'))
-
-
-
-# d[1,] %>% group_by(index) %>% summarize(m = mean(area), do_union=FALSE) %>% st_cast("MULTILINESTRING") %>% plot()
-
-d[1,] %>% st_cast('POINT') %>% st_geometry() %>% st_nearest_points(d[1,] %>% st_centroid())
-
-d[1:10,] %>% group_by(index) %>% st_cast('POINT') %>% st_geometry() %>% st_nearest_points(d[1:10, ] %>% st_centroid) %>% plot()
-
-map(1:10, function(x) d[x,] %>% st_cast('POINT') %>%
-      st_geometry() %>% st_nearest_points(d[x,] %>% st_centroid())) %>% do.call(rbind, .)
-
-a <- map_dfr(d$index[1:10], function(x) {
-  r <- d[d$index == x,] %>% st_cast('POINT') %>%
-    st_geometry() %>%
-    st_nearest_points(d[d$index == x,] %>% st_centroid()) %>%
-    st_as_sf() %>% 
-    st_set_geometry('geometry')
-  r$index <- x
-  return(r)
-})
-
-
-a <- map_dfr(ind_esp$h3_address, function(x) {
-  r <- ind_esp[ind_esp$h3_address == x,] %>% st_cast('POINT') %>%
-    st_geometry() %>%
-    st_nearest_points(ind_esp[ind_esp$h3_address == x,] %>% st_centroid()) %>%
-    st_as_sf() %>% 
-    st_set_geometry('geometry')
-  r$index <- x
-  return(r)
-})
-
-# library(stplanr)
-# line_segment(a, segment_length = units(10, 'm'))
-st_line_sample(a %>% st_transform(32619), density = set_units(10, 'm')) %>% st_transform(4326) %>% st_write('sep-10-metros.kml')
-
-generar_radiales <- function(objeto_espacial = NULL, columna_indice = NULL) {
+generar_radios <- function(objeto_espacial = NULL, columna_indice = NULL) {
   library(sf)
   library(tidyverse)
-  objeto_interno <- objeto_espacial %>% st_geometry()
-  objeto_interno$indice <- objeto_interno[, columna_indice, drop=T] %>% st_drop_geometry()
-  return(objeto_interno)
-  r_sf <- map_dfr(unique(objeto_interno$indice), function(x) {
-    r <- objeto_interno[objeto_interno$indice == x,] %>% st_cast('POINT') %>%
-      st_geometry() %>%
-      st_nearest_points(objeto_interno[objeto_interno$indice == x,] %>% st_centroid()) %>%
+  objeto_interno <- objeto_espacial %>% st_geometry() %>% st_as_sf() %>% st_set_geometry('geometry')
+  indice <- objeto_espacial[[columna_indice]]
+  # attr(indice, 'class') <- NULL
+  objeto_interno <- objeto_interno %>% mutate(indice = indice)
+  radios_sf <- map_dfr(unique(objeto_interno$indice), function(x) {
+    puntos <- objeto_interno[objeto_interno$indice == x,] %>%
+      st_cast('POINT') %>%
+      distinct() %>% 
+      st_geometry()
+    centroides <- objeto_interno[objeto_interno$indice == x,] %>% st_centroid()
+    radios <- centroides %>% st_nearest_points(puntos) %>%
       st_as_sf() %>% 
       st_set_geometry('geometry')
-    r$indice <- x
-    return(r)
+    radios[, columna_indice] <- x
+    id_radio_num <- 1:nrow(radios)
+    id_radio_acolchado <- stri_pad_left(id_radio_num,
+                                        max(nchar(id_radio_num)), 0)
+    radios$id_radio <- paste0(radios[[columna_indice]], 'r', id_radio_acolchado)
+    return(radios)
   })
-  return(r_sf)
+  return(radios_sf)
 }
